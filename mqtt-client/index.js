18fa1a5315ddd9e54de1906475e0ddf842818fd3
Murilo Amêndola <muriloamendola@gmail.com>
2018-02-18 17:05:07 -0300

    Primeiro commit apenas com um exemplo de client mqtt em nodejs

---
+++ mqtt-client/index.js
@@ -0,0 +1,7 @@
+const mqtt = require('mqtt');
+const client  = mqtt.connect({ host: 'iot.eclipse.org', port: 1883 });
+ 
+client.on('connect', () => {
+  console.log('Conectado!')
+  client.publish('29asoIoTMIL', '0')
+})
