b81f8873f9e340b4ef42e9a29dd7047ffcbeade9
Murilo Amêndola <muriloamendola@gmail.com>
2018-02-18 18:12:25 -0300

    Capturando temperatura a cada 2 segundos

--- raspberrypi/temperature.py
+++ raspberrypi/temperature.py
@@ -2,6 +2,7 @@ import sys
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import Adafruit_DHT
+import time
 
 LED_PIN_NUMBER = 27
 
@@ -37,6 +38,7 @@ try:
   while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+    time.sleep(2)
 
   # MQTT Client to control LED status ON/OFF
   client = mqtt.Client()
