d863b6ccaac41c7b04bf8e9815e0a94071ae0c23
Murilo Amêndola <muriloamendola@gmail.com>
2018-02-18 17:41:50 -0300

    Refactoring temperature.py

---
+++ raspberrypi/temperature.py
@@ -0,0 +1,45 @@
+import sys
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+LED_PIN_NUMBER = 27
+
+# BROKER properties
+BROKER = 'iot.eclipse.org'
+BROKER_PORT = 1883
+KEEP_ALIVE_BROKER = 60
+TOPIC = '29asoIoTMIL'
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(LED_PIN_NUMBER, GPIO.OUT)
+
+def led_on():
+  GPIO.output(LED_PIN_NUMBER, GPIO.HIGH)
+
+def led_off():
+  GPIO.output(LED_PIN_NUMBER, GPIO.LOW)
+
+def on_connect(client, userdata, flags, rc):
+  print('Conectado ao BROKER: ' + str(rc))
+  client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+  payload = str(msg.payload.decode('utf-8'))
+  print('Mensagem recebida: ' + payload)
+  if (payload == 'ON'):
+    led_on()
+  elif (payload == 'OFF'):
+    led_off()
+  
+try:
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+
+  client.connect(BROKER, BROKER_PORT, KEEP_ALIVE_BROKER)
+  client.loop_forever()
+except KeyboardInterrupt:
+  print('\nCtrl+c pressionado, encerrando aplicacao...')
+  sys.exit(0)
+
+ 
