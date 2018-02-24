b1bafc15a45ea7fc470c5265bceaad857e1754ea
Murilo Amêndola <muriloamendola@gmail.com>
2018-02-24 11:34:43 -0300

    Adicionando regra para enviar a temperatura lida pelo sensor para um serviço web

--- iot/raspberrypi/temperature.py
+++ iot/raspberrypi/temperature.py
@@ -1,6 +1,18 @@
 import sys
 import Adafruit_DHT
 import time
+import requests
+import json
+
+def send_data_to_service(temperature, humidity):
+  service_url = 'https://15m3sva997.execute-api.us-east-1.amazonaws.com/dev/temperature'
+  data = json.dumps({ 
+    'temperature': temperature,
+	  'humidity': humidity,
+	  'markingAt': time.time()
+  })
+  response = requests.post(service_url, data)
+  print(response.json())
 
 try:
   while True:
